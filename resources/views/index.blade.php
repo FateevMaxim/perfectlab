8bcc07bec53fc1e6fee2a02ab4d756bf98bc7883
FateevMaxim <fateev.maxim@gmail.com>
2021-08-25 15:17:59 +0600

    Аналитика Яндекс

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -785,7 +785,7 @@
             <h3>PerfectLab</h3>
             <p>
               г. Алматы, <br />ул. Прокофьева, д.89А <br><br>
-              <strong>Phone:</strong> +7 <br>
+              <strong>Телефон:</strong> +7 <br>
               <strong>Email:</strong> info@perfectlab.kz<br>
             </p>
           </div>
@@ -829,7 +829,21 @@
 
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
+  <!-- Yandex.Metrika counter -->
+  <script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
+    ym(84467023, "init", {
+      clickmap:true,
+      trackLinks:true,
+      accurateTrackBounce:true,
+      webvisor:true
+    });
+  </script>
+  <noscript><div><img src="https://mc.yandex.ru/watch/84467023" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+  <!-- /Yandex.Metrika counter -->
 </body>
 
 </html>
